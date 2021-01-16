0acc28ecdd919c700b4618d7f892c193c35d6df5
Drewbs <drewbassett24@gmail.com>
2021-01-16 21:32:48 +0000

    CSS and HTML Completed. Some vars created

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//Moment
+var dateTimeNow = moment().format("MMMM do YYY, h:mm:ss a");
+
+var nineAM = $("#9AM");
+var tenAM = $("#10AM");
+var elevenAM = $("#11AM");
+var twelveAM = $("#12AM");
+var onePM = $("#1PM");
+var twoPM = $("#2PM");
+var threePM = $("#3PM");
+var fourPM = $("#4PM");
+var fivePM = $("#5PM");
+var sixPM = $("6PM");
+
+var hour = moment ().hours();
+var userInput;
+var hourSpan;
+
+var interval = setInterval(function() {
+    
+}
