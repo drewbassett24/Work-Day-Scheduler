481a6479db8e9ceff722a6f2777e3c134dc82ae0
Drewbs <drewbassett24@gmail.com>
2021-01-19 23:55:40 +0000

    wrote new variables; wrote functions to record the schedule

--- js/script.js
+++ js/script.js
@@ -1,22 +1,58 @@
-//Moment
-var dateTimeNow = moment().format("MMMM do YYY, h:mm:ss a");
+//Declare Vars
+var currentDay = $("#currentDay");
+var scheduleArea = $(".schedule");
+var timeRow = $(".timeRow");
+var currentDate = moment().format("ddd, MMM Do");
+var currenthour = moment ().format("H");
+var toDos = [];
 
-var nineAM = $("#9AM");
-var tenAM = $("#10AM");
-var elevenAM = $("#11AM");
-var twelveAM = $("#12AM");
-var onePM = $("#1PM");
-var twoPM = $("#2PM");
-var threePM = $("#3PM");
-var fourPM = $("#4PM");
-var fivePM = $("#5PM");
-var sixPM = $("6PM");
+function startSchedule(){
+    timeRow.each(function(){
+        var thisRow = $(this);
+        var thisRowHr = parseInt(thisRow.attr("data-hour"));
+        var toDoObject = {
+            hour: thisRowHr,
+            text: "",
+        }        
+        toDoItems.push(toDoObj);
+    });
 
-var hour = moment ().hours();
-var userInput;
-var hourSpan;
+    localStorage.setItem("todos", JSON.stringify(toDoItems));
+};
 
-var interval = setInterval(function() {
-    var momentNow = moment();
-    $
-}, 100)
+function saveIt(){
+    var hourToUpdate = $(this).parent().attr("data-hour");
+    var itemToHold = (($(this).parent()).children("textarea")).val();
+    for (var j = 0; j < toDoItems.length; j++){
+        if (toDoItems[j].hour == hourToUpdate){
+            toDoItems[j].text = itemToAdd;
+    }
+}
+
+
+
+
+
+
+
+// var dateTimeNow = moment().format("MMMM do YYY, h:mm:ss a");
+
+// var nineAM = $("#9AM");
+// var tenAM = $("#10AM");
+// var elevenAM = $("#11AM");
+// var twelveAM = $("#12AM");
+// var onePM = $("#1PM");
+// var twoPM = $("#2PM");
+// var threePM = $("#3PM");
+// var fourPM = $("#4PM");
+// var fivePM = $("#5PM");
+// var sixPM = $("6PM");
+
+// var hour = moment ().hours();
+// var userInput;
+// var hourSpan;
+
+// var interval = setInterval(function() {
+//     var momentNow = moment();
+//     $
+// }, 100)
